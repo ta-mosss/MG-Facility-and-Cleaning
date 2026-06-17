57295136afdb3f998ad346ebedef63fa6c31f35d
Moses Mbulaheni <moses.m@mbulahenigroup.co.za>
2026-06-17 20:02:17 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('mgfc-v1').then(cache => {
+      return cache.addAll(['/', '/index.html']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
